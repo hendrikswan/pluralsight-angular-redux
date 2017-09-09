0dbacd0e969d7497089d8df14ec992fc56f31109
Alexey Sednev <asednev@metromile.com>
2017-09-08 17:54:17 -0700

    Fix spinner unit test

--- src/app/blocks/spinner/spinner.component.spec.ts
+++ src/app/blocks/spinner/spinner.component.spec.ts
@@ -2,10 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { SpinnerComponent } from './spinner.component';
+import { SpinnerService } from './spinner.service';
 
 describe('Component: Spinner', () => {
   it('should create an instance', () => {
-    let component = new SpinnerComponent();
+    let component = new SpinnerComponent(new SpinnerService());
     expect(component).toBeTruthy();
   });
 });
